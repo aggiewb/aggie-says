a8780dcbec109382e6ccfa064a8942870fb894aa
AggieWB <usmaggie23@gmail.com>
2020-04-26 15:31:01 -0700

    Add const that contains an object of color attributes to mp3 files

--- js/aggie-says.js
+++ js/aggie-says.js
@@ -1,3 +1,11 @@
+var SOUNDS = {
+    blue: new Audio("media/cat.mp3"),
+    yellow: new Audio("media/goat.mp3"),
+    green: new Audio("media/turkey.mp3"),
+    red: new Audio("media/dog.mp3"),
+    incorrect: new Audio("media/fart.mp3")
+};
+
 //Function that highlight a button
 function toggleColorClass(element){
     var color = element.dataset.color;
